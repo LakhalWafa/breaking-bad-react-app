81e9a7e777084f91c60bc03f5ed1441a0a13f76e
Lakhal Wafa <lakhal.wafa.3@gmail.com>
2020-07-22 12:38:46 +0100

    first commit

---
+++ src/components/ui/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../../img/logo.png";
+
+const Header = () => {
+  return (
+    <Header className="center">
+      <img src={logo} alt="" />
+    </Header>
+  );
+};
+
+export default Header;
